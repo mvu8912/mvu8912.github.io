98c6fe46b2d46ccd8fb6c7ea1bffd81cdc67dec5
Michael Vu <email@michael.vu>
2024-02-10 17:51:13 +0000

    bugfix

--- js/tilt-manual-mode.js
+++ js/tilt-manual-mode.js
@@ -1,45 +1,25 @@
-function manual(service) {
-    if (service == null) {
-        let matches = location.href.match(/&manual=([^&]+)/);
-        if (matches) {
-            service = matches[1];
-        }
-        else {
-            console.log('No service was provided.');
-            return;
-        }
-    }
-    $(document).ready(() => {
-        // Find all <tr> elements, then filter based on the presence of a button with text 
-        $('tr').each(tr => {
-            let $row = $(tr.target); // Cache the current <tr>
-            
-            // Check if there's a button with text  in this <tr>
-            let hasServiceLabel = $row.find('button').filter((i, btn) => {
-                let label = $(btn).text().trim();
-                if (label == null) return false;
-                else return label == service;
-            }).length > 0;
-            
-            // If found, proceed to find the toggle
-            if (!hasServiceLabel) return;
+setInterval(() => {
+    let service = 'SOMETHING';
+    let tilt_url = 'http://localhost:10350/api/override';
 
-            // Find the toggle button within the same <tr>
-            // We're looking for a button without the specific title attribute
-            let $toggle = $row.find('button').filter((i, toggle) => {
-                let title = $(toggle).attr('title');
-                if (!title) return false;
-                else if (title.match(/Auto\:/)) return false;
-                else return true;
-            });
-            
-            // If the toggle button is found and it's not in manual mode, click it
-            if ($toggle.length > 0) {
-                $toggle.click();
-                console.log('Toggle clicked because it was not in manual mode.');
-            } else {
-                console.log('No toggle needing a click was found.');
+    $('tr').each((i, tr) => {
+        let $row = $(tr); // Cache the current <tr>
+        // Check if there's a button with text  in this <tr>
+        let hasServiceLabel = $row.find('button').filter((i, btn) => {
+            let label = $(btn).text().trim();
+            if (!label) return false;
+            else return label == service;
+        }).length > 0;
+        // If found, proceed to find the toggle
+        if (!hasServiceLabel) return;
+        $.ajax({
+            url: tilt_url + '/trigger_mode',
+            type: 'POST',
+            data: JSON.stringify({"manifest_names":[service],"trigger_mode":2}),
+            contentType: 'application/json; charset=utf-8',
+            success: function(result) {
+                console.log("Service: " + service + " has switched to manual mode");
             }
         });
     });
-}
+}, 5000);
